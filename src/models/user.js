72ee1404da380fcadf423b3031e9eecccbdd940e
Abhiraj <kumarabhiraj88@gmail.com>
2025-02-18 11:59:38 +0530

    env file added to gitignore

--- src/models/user.js
+++ src/models/user.js
@@ -3,15 +3,19 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     firstName: {
         type: String,
+        required: true
     },
     lastName:{
         type: String,
     },
     emailId:{
         type: String,
+        required: true,
+        unique: true
     },
     password: {
         type: String,
+        required: true
     },
     age:{
         type: Number
